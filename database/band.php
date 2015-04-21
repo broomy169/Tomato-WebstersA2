df4766f592f2878fe46dacff47b9b6b0ed10f2a6
Parminder Singh <preetzdomain@gmail.com>
2015-04-22 02:18:51 +1000

    fixed expand button for band.php working correctly now

--- database/band.php
+++ database/band.php
@@ -2,39 +2,33 @@
 //added connection
 include("inc_dbconnect.php");
 ?>
-
 <html>
 <head>
     <meta charset="UTF-8">
     <title>Band and Artists - TCMC</title>
     <link href="styleband.css" type="text/css" rel="stylesheet">
-    <script src='onloadband.js' type='text/javascript'></script>
-
 </head>
 <body>
-<h1>Artists and Bands</h1>
 <?php $sql = "SELECT * FROM band";
 $blockTally = 1;
 foreach($dbh->query($sql) as $row){
-    $image = "$row[band_promopic]";
-    $bandId = "$row[band_id]";
-    // band block starts here to style each band/artist information
-    echo "<div id='bandBlock$blockTally'>\n";
-    echo "<div id='infoBlock'>\n<p><redcolor>$row[band_name]</redcolor></p><p>short bio:
- <redcolor>$row[band_shortbio]</redcolor></p>";
-    echo "<div id='moreinfoBlock$blockTally'>\n<h3>Contact details</h3><p>phone: <redcolor>$row[band_phone]</redcolor></p><p>email:
-<redcolor>$row[band_email]</redcolor></p><p>website: <redcolor><a href='#'> $row[band_website]</a></redcolor></p>\n";
+
+    echo "<div id='band$blockTally'>\n";
+    echo "<div id='info'>\n<h2>$row[band_name]</h2>\n<h3>$row[band_shortbio]</h3>\n";
+
+    echo "<div id='moreInfo$blockTally' class='moreInfo'>\n<ul>\n<li>$row[band_phone]</li>\n<li>$row[band_email]
+</li>\n<li><a href=$row[band_website]>$row[band_website]</a></li>\n</ul>\n";
+
     echo "</div>\n";
     echo "</div>\n";
-    echo "<div id='photoBlock'>\n<h2>icon here</h2>\n";
-    echo "<div id='morephotoBlock$blockTally'>\n<h2>more photo/Photos? here</h2>\n";
+    echo "<div id='icon'>\n<h2>icon here</h2>\n";
+    echo "<div id='photo$blockTally' class='photo'>\n<h2>more photo/Photos? here</h2>\n";
     echo "</div>\n";
     echo "</div>\n";
     echo "<div id='clearBlock'>\n";
     echo "</div>\n";
-    echo "<input type='button' onclick='expand($blockTally);' id='moreinfo$blockTally' value='more info...'/input>\n";
+    echo "<input type='button' onclick='expand($blockTally);' id='button$blockTally' class='expandButton' value='more info...'/input>\n";
     echo "<script src='expandband.js' type='text/javascript'></script>";
-    // band block finish here
     echo "</div>\n";
     $blockTally++;
 }?>
--- database/expandband.js
+++ database/expandband.js
@@ -1,18 +1,18 @@
 // script is used to expand band/artist information when more info/less info button is clicked in band.php
 function expand(tally){
-    var button = document.getElementById("moreinfo" + tally);
-    var moreinfoblock = document.getElementById("moreinfoBlock" + tally);
-    var morephotosblock = document.getElementById("morephotoBlock" + tally);
+    var button = document.getElementById("button" + tally);
+    var moreInfo = document.getElementById("moreInfo" + tally);
+    var photo = document.getElementById("photo" + tally);
     console.log(tally); //testing purpose
 
     if (button.value === "more info..."){
         button.value = "less info..";
-        moreinfoblock.style.display = "block";
-        morephotosblock.style.display = "block";
+        moreInfo.style.display = "block";
+        photo.style.display = "block";
 
     } else {
         button.value = "more info...";
-        moreinfoblock.style.display = "none";
-        morephotosblock.style.display = "none";
+        moreInfo.style.display = "none";
+        photo.style.display = "none";
     }
 }
