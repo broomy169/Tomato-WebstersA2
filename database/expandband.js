292d8d231b6136cc50f5905f7d0b13cf2b14da55
Parminder Singh <preetzdomain@gmail.com>
2015-04-26 01:45:00 +1000

    removed button and added click anywhere function on band list to expand

--- database/expandband.js
+++ database/expandband.js
@@ -1,18 +1,18 @@
-// script is used to expand band/artist information when more info/less info button is clicked in band.php
+// script is used to expand band/artist information when clicked on any of it
 function expand(tally){
-    var button = document.getElementById("button" + tally);
+    var click = document.getElementById("band" + tally);
     var moreInfo = document.getElementById("moreInfo" + tally);
     var photo = document.getElementById("photo" + tally);
     console.log(tally); //testing purpose
 
-    if (button.value === "more info..."){
-        button.value = "less info..";
-        moreInfo.style.display = "block";
-        photo.style.display = "block";
-
-    } else {
-        button.value = "more info...";
+    if (click.value === "show"){
+        click.value = "hide";
         moreInfo.style.display = "none";
         photo.style.display = "none";
+
+    } else {
+        click.value = "show";
+        moreInfo.style.display = "block";
+        photo.style.display = "block";
     }
 }
