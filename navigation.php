7784a96dc60ced49c51467d2c9dc26eb949b4443
Graeme Broomhall <jc258876@sa01-013a-w04.ad.jcu.edu.au>
2015-04-28 20:14:39 +1000

    Added navigation page for sql queries

---
+++ navigation.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <title>Tomato Websters - Navigation</title>
+</head>
+<body>
+   	<header><h1>Navigation</h1></header>
+    <a href="bands.php"><h2>Display summary/full decription of artists</h2></a>
+    <a href="database/manageband.php"><h2>Manage artists(Add/Update/Delete)</h2></a>
+<?php
+	include("database/band.php");
+	?>
+</body>
+</html>
