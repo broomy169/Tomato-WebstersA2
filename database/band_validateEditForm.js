bdf41d424bc70cc3c5750d15f57101e9b2607ae3
kurt <kurt.schoenhoff@my.jcu.edu.au>
2015-05-08 17:35:35 +1000

    added helper functions. TODO: include Validation_HelperFunctions.js in all pages with forms

--- database/band_validateEditForm.js
+++ database/band_validateEditForm.js
@@ -5,7 +5,29 @@ function validateEditForm(tally) {
     var emailE = document.forms["editRecord" + tally]["band_email"].value;
     var iconE = document.forms["editRecord" + tally]["iconfile"].value;
     var imageE = document.forms["editRecord" + tally]["imagefile"].value;
+	
+	
+	// check the form values using Validation_HelperFunctions.js <-- make sure this file is included in the page!!!
+	//
+	//the following code tallys the score and returns false if there is a problem
+	
+	
+	tally = tally + validateDoesExist(nameE,"band name.");
+	tally = tally + validateDoesExist ((text === "emptygenre" ? text = "" : text = text), msg); // if genre === "emptygenre" change to "" to trigger error in validateDoesExist
+	tally = tally + validatePhone(phoneE);
+	tally = tally + validateEmail(emailE);
+	tally = tally + validateDoesExist(iconE, " choose an icon image.");
+	tally = tally + validateDoesExist(imageE, " choose large image.");
+	
+	if (tally > 0)
+		return false;
+	else
+		return true;
+	
+	
+	
 
+	/*		----------OLD codezzz --------------
     if (nameE === "") {
         alert ("Please enter band name.");
         return false;
@@ -30,4 +52,5 @@ function validateEditForm(tally) {
     } else {
         return true;
     }
+	*/
 }
