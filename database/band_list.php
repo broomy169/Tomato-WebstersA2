e5052e9bc573d169530faf6820223ed74e24c1dc
kurt <kurt.schoenhoff@my.jcu.edu.au>
2015-05-10 02:34:30 +1000

    merged bandlist css into styles.css

--- database/band_list.php
+++ database/band_list.php
@@ -4,7 +4,7 @@
     isset($urlVar) || $urlVar = "";
     include($urlVar . "database_connect.php");
 
-    echo "<link rel='stylesheet' href= 'database/band_listStyle.css' type='text/css'>";
+    //echo "<link rel='stylesheet' href= 'database/band_listStyle.css' type='text/css'>"; // now merged with styles.css
     echo "<script src= 'database/band_expandInfo.js' type='text/javascript'></script>";
 
     $sql = "SELECT * FROM band";
@@ -51,26 +51,35 @@
                     </div>
                     
                     <!-- End of always show -->
+                    
                     <!-- Start of expanded view -->
                     
                     <div id='info-more$blockTally' class='info-more'>
-                        <p>$row[band_longBio]</p>
-                        <ul>
-                            <li><p>'contact us on this phone number'>Phone: $row[band_phone]</p></li>
-                            <li><p>'click here to email us'>Email: <a href='mailto:$row[band_email]'>$row[band_email]</a></p></li>
-                            <li><p>'click here to go to our website'>Website: <a target='_blank' href=$row[band_website]>$row[band_website]</a></p></li>
-                        </ul>";
+                        <div class='col w-2col m-2col '>
+                            <h2>$row[band_name]</h2>
+                            <h3>$row[band_shortBio]</h3>
+                            <p>$row[band_longBio]</p>
+                            <ul>
+                                <li><p>'contact us on this phone number'>Phone: $row[band_phone]</p></li>
+                                <li><p>'click here to email us'>Email: <a href='mailto:$row[band_email]'>$row[band_email]</a></p></li>
+                                <li><p>'click here to go to our website'>Website: <a target='_blank' href=$row[band_website]>$row[band_website]</a></p></li>
+                            </ul>
+                        </div>";
 
 
         //checking and showing if image already added by user otherwise displaying default image
         if (file_exists($imageUrl)){
-            echo "      <div id='photo$blockTally' class='photo'>
-                            <img src='$imageUrl'>
+            echo "      <div class='col w-2col m-2col '>
+                            <div class='imgBox100'>
+                                <div class='row-fixedHeight'>
+                                    <img id='photo$blockTally' src='$imageUrl'>
+                                </div>
+                            </div>
                         </div>";
         } 
         else {
             echo "      <div id='photo$blockTally' class='photo'>
-                            <img src='database/images/defaultTomatoLight.jpg'>
+                            <img id='photo$blockTally' src='database/images/defaultTomatoLight.jpg'>
                         </div>";
         }
 
