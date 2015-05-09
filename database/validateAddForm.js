fdcb6b7d207e7f8f600448151b2a40e066961d90
kurt <kurt.schoenhoff@my.jcu.edu.au>
2015-05-09 10:02:36 +1000

    fixed index page CTA resizing

---
+++ database/validateAddForm.js
@@ -0,0 +1,34 @@
+function validateAddForm() {
+    var name = document.forms["addRecord"]["band_name"].value;
+    var genre = document.forms["addRecord"]["band_genre"].value;
+    var phone = document.forms["addRecord"]["band_phone"].value;
+    var email = document.forms["addRecord"]["band_email"].value;
+    var icon = document.forms["addRecord"]["iconfile"].value;
+    var image = document.forms["addRecord"]["imagefile"].value;
+
+    if (name === "") {
+        alert ("Please enter band name.");
+        return false;
+    } else if (genre === "emptygenre") {
+        alert("Please choose genre.");
+        return false;
+    } else if (phone === "" || isNaN(phone)) {
+        alert("Please enter valid phone number.");
+        return false;
+
+    } else if (email === "") {
+        alert("Please enter email address.");
+        return false;
+
+    } else if (icon === "" || icon === null) {
+        alert("Please choose both icon and image, uploading icon only not allowed.");
+        return false;
+
+    } else if (image === "" || icon === null) {
+        alert ("Please choose both image and icon, uploading image only not allowed.");
+        return false;
+    } else {
+        return true;
+    }
+
+}
