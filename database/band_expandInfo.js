1382d49d40b397a5dd06093c5f8b33393e114438
kurt <kurt.schoenhoff@my.jcu.edu.au>
2015-05-10 00:56:35 +1000

    fixing up html in band_list.php

--- database/band_expandInfo.js
+++ database/band_expandInfo.js
@@ -1,11 +1,11 @@
 // script is used to expand band/artist information when clicked on any of it
-function expand(tally){
+function expand(tally) {
     var click = document.getElementById("band" + tally);
     var moreInfo = document.getElementById("moreInfo" + tally);
     var photo = document.getElementById("photo" + tally);
     console.log(tally); //testing purpose
 
-    if (click.value === "show"){
+    if (click.value === "show") {
         click.value = "hide";
         click.title = "click here for more information";
         moreInfo.style.display = "none";
--- database/band_list.php
+++ database/band_list.php
@@ -1,52 +1,83 @@
 <?php
-//including connection code for database
-//following code will make this file accessible from other directories as well
-isset($urlVar) || $urlVar = "";
-include($urlVar . "database_connect.php");
+    //including connection code for database
+    //following code will make this file accessible from other directories as well
+    isset($urlVar) || $urlVar = "";
+    include($urlVar . "database_connect.php");
 
-echo "<link rel='stylesheet' href= 'database/band_listStyle.css' type='text/css'>";
-echo "<script src= 'database/band_expandInfo.js' type='text/javascript'></script>";
+    echo "<link rel='stylesheet' href= 'database/band_listStyle.css' type='text/css'>";
+    echo "<script src= 'database/band_expandInfo.js' type='text/javascript'></script>";
 
-$sql = "SELECT * FROM band";
-// counter or tally that helps to distinguish between each record's tags/click/input in JavaScript
-$blockTally = 1;
+    $sql = "SELECT * FROM band";
+    // counter or tally that helps to distinguish between each record's tags/click/input in JavaScript
+    $blockTally = 1;
 
-//loop going through each band record and displays its info
-foreach($dbh->query($sql) as $row){
+    //loop going through each band record and displays its info
+    foreach($dbh->query($sql) as $row){
 
-    // setting up and storing url links for icon and image
-    $iconUrl = $urlVar . $row['band_promoIcon'];
-    $imageUrl = $urlVar . $row['band_promoPic'];
+        // setting up and storing url links for icon and image
+        $iconUrl = $urlVar . $row['band_promoIcon'];
+        $imageUrl = $urlVar . $row['band_promoPic'];
 
-    echo "<div id='band$blockTally' class='bandClass' onclick='expand($blockTally);' title='click here for more information' value='hide'>\n";
-    echo "<div id='info'>\n<h2>$row[band_name]</h2>\n<h3>$row[band_shortBio]</h3>\n";
-    echo "<div id='moreInfo$blockTally' class='moreInfo'>\n<p>$row[band_longBio]</p>\n<ul>\n<li><p
-title='contact us on this phone number'>Phone: $row[band_phone]</p></li>\n
-<li><p title='click here to email us'>Email: <a href='mailto:$row[band_email]'>$row[band_email]</a></p></li>\n
-<li><p title='click here to go to our website'>Website: <a target='_blank' href=$row[band_website]>$row[band_website]</a></p></li>\n";
-    echo "</div>\n";
-    echo "</div>\n";
+        echo" 
+            <!-- Start of single band List item-->
+            <li>
+                <div id='band$blockTally' class='bandClass' onclick='expand($blockTally);' title='click here for more information' value='hide'>
+                
+                    <!-- Start of always show -->
+                    
+                    <div id='info'>
+                        <h2>$row[band_name]</h2>
+                        <h3>$row[band_shortBio]</h3>";
+                        
+                        //checking if image exists and display
+        if (file_exists($iconUrl)){
+            echo "      <div id='icon'>
+                            <img src='$iconUrl'>
+                        </div>";
+        } 
+        else {
+            echo "      <div id='icon'>
+                            <img src='database/images/defaultTomato.jpg'>
+                        </div>";
 
-    //checking and showing if image already added by user otherwise displaying default image
-    if (file_exists($iconUrl)){
-        echo "<div id='icon'>\n<h2><img src='$iconUrl'></h2>\n";
-    } else {
-        echo "<div id='icon'>\n<h2><img src='database/images/defaultTomato.jpg'></h2>\n";
-    }
-    //echo "<div id='icon'>\n<h2><img src='$iconUrl'></h2>\n";
+        }
+         echo"           
+                    </div>
+                    
+                    <!-- End of always show -->
+                    <!-- Start of expanded view -->
+                    
+                    <div id='moreInfo$blockTally' class='moreInfo'>
+                        <p>$row[band_longBio]</p>
+                        <ul>
+                            <li><p>'contact us on this phone number'>Phone: $row[band_phone]</p></li>
+                            <li><p>'click here to email us'>Email: <a href='mailto:$row[band_email]'>$row[band_email]</a></p></li>
+                            <li><p>'click here to go to our website'>Website: <a target='_blank' href=$row[band_website]>$row[band_website]</a></p></li>
+                        </ul>";
 
-    //checking and showing if image already added by user otherwise displaying default image
-    if (file_exists($imageUrl)){
-        echo "<div id='photo$blockTally' class='photo'>\n<li><h2><img src='$imageUrl'></h2>\n";
-    } else {
-        echo "<div id='photo$blockTally' class='photo'>\n<li><h2><img src='database/images/defaultTomatoLight.jpg'></h2>\n";
-    }
 
-    echo "</div>\n";
-    echo "</div>\n";
-    echo "<div id='clearBlock'><li>\n";
-    echo "</div>\n";
-    echo "</div>\n";
-    $blockTally++;
+        //checking and showing if image already added by user otherwise displaying default image
+        if (file_exists($imageUrl)){
+            echo "      <div id='photo$blockTally' class='photo'>
+                            <img src='$imageUrl'>
+                        </div>";
+        } 
+        else {
+            echo "      <div id='photo$blockTally' class='photo'>
+                            <img src='database/images/defaultTomatoLight.jpg'>
+                        </div>";
+        }
+
+        echo "      </div>
+                    <div id='clearBlock'><li>
+                    </div>
+                    
+                    <!-- End of expanded view -->
+                    
+                </div>
+            </li>
+            
+            <!-- End of single band List item-->";
+        $blockTally++;
 }?>
 
