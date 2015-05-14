1185ed7041ee230d6376bb62a4ceff6efd0f9523
Parminder Singh <preetzdomain@gmail.com>
2015-05-14 11:30:25 +1000

    ...

---
+++ database/signup_form.php
@@ -0,0 +1,22 @@
+<?php
+isset($urlVar) || $urlVar = "";
+include($urlVar . "database_connect.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>SignUp - TCMC</title>
+</head>
+<body>
+<h1>Sign Up</h1>
+<form>
+
+
+
+</form>
+
+
+
+</body>
+</html>
