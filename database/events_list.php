5a25f9c84d972223c24cb255e840484d03e27358
kurt <kurt.schoenhoff@my.jcu.edu.au>
2015-05-17 11:15:08 +1000

    modifying to get expanded view correct

--- database/events_list.php
+++ database/events_list.php
@@ -28,7 +28,8 @@
                 
                     <!-- Start of always show -->
                    
-                    <div id='info-small$blockTally'>";
+                    <div id='info-small$blockTally'>
+        ";
                         
                         //checking if image exists and display
         if (file_exists($iconUrl)){
@@ -39,30 +40,69 @@
                                     <img id='icon$blockTally'src='$iconUrl' alt='$row[event_title]'>
                                 </div>
                             </div>
-                        </div>";
+                        </div>
+            ";
         } 
         else {
             echo "
-                        <div id='icon'>
-                            <img src='database/images/defaultTomato.jpg'>
-                        </div>";
+                        <div class='col w-1col m-1col float-Left'>
+                            <div class='imgBox100'>
+                                <div class='row-fixedHeight'>
+                                    <img id='icon$blockTally'src='database/images/defaultTomato.jpg' alt='$row[event_title]'>
+                                </div>
+                            </div>
+                        </div>
+        ";
 
         }
         echo" 
 
                         
                         <div class='col w-1col m-1col float-Right'>
-                            <h3>Members</h3>";
-        //need to pull event members from database and into an array and foreach into a list
+                            <h3>$row[event_date]</h3>
+        ";
+        //need to pull event venue details from database 
         echo"
-                            <ul>
-                                <li>member1</li>
-                                <li>member2</li>
-                                <li>member3</li>
-                            </ul>";
+                            <ul>";
+        if(!(empty($row['event_venueName']))){
+            echo"              
+                                <li><p>Venue: $row[event_venueName]</p></li>
+            ";
+        }
+        else{
+            echo"              
+                                <li><p>Venue: Not Entered</p></li>
+            ";
+        }
+                                
+        if(!(empty($row['event_venueLocation']))){
+            echo"              
+                                <li><p>Location: $row[event_venueLocation]</p></li>
+            ";
+        }
+        else{
+            echo"              
+                                <li><p>Location: Not Entered</p></li>
+            ";
+        }
+                                
+        if(!(empty($row['event_priceFull']))){
+            echo"              
+                                <li><p>Full Price: $row[event_priceFull]</p></li>
+            ";
+        }
+        else{
+            echo"              
+                                <li><p>Full Price: Not Entered</p></li>
+            ";
+        }
+        echo"               </ul>
+        ";
         
                             
-       echo"            </div>
+       echo"            
+                        </div>
+                        
                         <!--<div class='col w-2col m-2col '>-->
                             <h2>$row[event_title]</h2>
                             <h4>$row[event_shortBio]</h4>
@@ -75,46 +115,115 @@
                     
                     <!-- Start of expanded view -->
                     
-                    <div id='info-more$blockTally' class='info-more'>";
+                    <div id='info-more$blockTally' class='info-more'>
+        ";
                 //checking and showing if image already added by user otherwise displaying default image
         if (file_exists($imageUrl)){
-            echo "      <div class='col w-2col m-2col float-Right'>
+            echo "      
+                        <div class='col w-2col m-2col float-Right'>
                             <div class='imgBox100'>
                                 <div class='row-fixedHeight'>
                                     <img id='photo$blockTally' src='$imageUrl'>
                                 </div>
                             </div>
-                        </div>";
+                        </div>
+            ";
         } 
         else {
-            echo "      <div id='photo$blockTally' class='photo'>
+            echo "      
+                        <div id='photo$blockTally' class='photo'>
                             <img id='photo$blockTally' src='database/images/defaultTomatoLight.jpg'>
-                        </div>";
+                        </div>
+            ";
         }
                     
         echo "
-                        <!--<div class='col w-2col m-2col '>-->
-                            <h2>$row[event_title]</h2>
-                            <h3>$row[event_shortBio]</h3><br>
-                            <p>$row[event_longBio]</p>
-                            <ul>";
+                        
+                        <h2>$row[event_title]</h2>
+                        <h3>$row[event_shortBio]</h3><br>
+                        <p>$row[event_longBio]</p>
+                            
+                        <div class='col w-2col m-2col '>
+                            <ul>
+        ";
+        
+        
+
+        
         if(!((empty($row['event_phone'])) or ($row['event_phone'] == "none"))){
-            echo"              <li><p>You can contact us on this phone number: $row[event_phone]</p></li>";
+            echo"              
+                                <li><p>You can contact us on this phone number: $row[event_phone]</p></li>
+            ";
         }
         if(!((empty($row['event_email']))or ($row['event_email'] == "none"))){
-            echo"              <li><p>or try email: <a href='mailto:$row[event_email]'>$row[event_email]</a></p></li>";
+            echo"              
+                                <li><p>or try email: <a href='mailto:$row[event_email]'>$row[event_email]</a></p></li>
+            ";
         }
         if(!((empty($row['event_website']))or ($row['event_website'] == "none"))){
-            echo"              <li><p>while your at it you may as well checkout our Website: <a target='_blank' href=$row[event_website]>$row[event_website]</a></p></li>";
+            echo"              
+                                <li><p>while your at it you may as well checkout our Website: <a target='_blank' href=$row[event_website]>$row[event_website]</a></p></li>
+            ";
         }
         
         
         echo"
                                 
                             </ul>
-                        <!--</div>-->";
+                        </div>";
+        
+        
+        echo" 
+        
+                        <div class='col w-2col m-2col '>
+                            <h3>$row[event_date]</h3>";
+        //need to pull event venue details from database 
+        echo"
+                            <ul>";
+        if(!(empty($row['event_venueName']))){
+            echo"              
+                                <li><p>Venue: $row[event_venueName]</p></li>
+            ";
+        }
+        else{
+            echo"              
+                                <li><p>Venue: Not Entered</p></li>
+            ";
+        }
+                                
+        if(!(empty($row['event_venueLocation']))){
+            echo"              
+                                <li><p>Location: $row[event_venueLocation]</p></li>
+            ";
+        }
+        else{
+            echo"              
+                                <li><p>Location: Not Entered</p></li>
+            ";
+        }
+                                
+        if(!(empty($row['event_priceFull']))){
+            echo"              
+                                <li><p>Full Price: $row[event_priceFull]</p></li>
+            ";
+        }
+        else{
+            echo"              
+                                <li><p>Full Price: Not Entered</p></li>
+            ";
+        }
+        echo"               
+                            </ul>
+                            ";
+        
+                            
+       echo"               
+                        </div>
+                        ";
+        
 
-        echo "      </div>
+        echo "      
+                    </div>
                     <div id='clearBlock'>
                     </div>
                     
