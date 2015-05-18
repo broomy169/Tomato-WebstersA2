70f16d58aca14789adced279a4b000cdfae2d543
kurt <kurt.schoenhoff@my.jcu.edu.au>
2015-05-18 19:14:43 +1000

    styles add events to use 2-col for entering data

--- database/event_manageList.php
+++ database/event_manageList.php
@@ -1,48 +1,55 @@
 <?php
-isset($urlVar) || $urlVar = "";
-include($urlVar . "database_connect.php");
+    isset($urlVar) || $urlVar = "";
+    include($urlVar . "database_connect.php");
 
 
-echo"
-    <div id='pageWrapper'>
-        <h1>Add Events</h1>
-        <form id='addEvent' name='addEvent' method='post' enctype='multipart/form-data' action='";echo"$urlVar";echo"event_databaseProcess.php'>
-            <fieldset>
-                <h2>Add new Event record:</h2>
-                
-                <span class='error'>* required fields</span></p>
-                <p><label for='event_title'>Event Name:- </label><input type='text' name='event_title' id='event_title' value=''><span class='error'>*</span></p>
-                <p><label for='event_phone'>Phone:- </label><input type='text' name='event_phone' id='event_phone' value=''><span class='error'>*</span></p>
-                <p><label for='event_date'>Date(dd/mm/yyyy):- </label><input type='text' name='event_date' id='event_date' value=''><span class='error'>*</span></p>
-                <p><label for='event_shortBio'>Short Bio:- </label><textarea type='text' name='event_shortBio' id='event_shortBio'></textarea></p>
-                <p><label for='event_longBio'>Long Bio:- </label><textarea type='text' name='event_longBio' id='event_longBio'></textarea></p>
-                <p><label for='file'>Upload Icon/Logo:</label><input type='file' name='iconfile' id='iconfile' value=''><span class='error'>*</span></p>
-                <p><label for='file'>Upload Image:</label><input type='file' name='imagefile' id='imagefile' value=''><span class='error'>*</span></p>
-                
-                <input type='submit' name='submit' id='submit' value='Add Event'>
-                
-                <div id='box' title='Things to note'>
-                    <label><p>Note:</p> </label>
-                    <p> - Individual upload for icon or photo not allowed. When adding record, both photo and icon must be uploaded on same time. </p>
-                </div>
-                
-            </fieldset>
-        </form>
-";
-    
-    echo "<h1>Current Event data/Information:</h1>";
+    echo"
+        <div class='col w-2col m-2col'>
+            <h1>Add Events</h1>
+            <form id='addEvent' name='addEvent' method='post' enctype='multipart/form-data' action='";echo"$urlVar";echo"event_databaseProcess.php'>
+                <fieldset>
+                    <h2>Add new Event record:</h2>
 
-        $sql = "SELECT * FROM Events";
-        $result = $dbh->query($sql);
-        $rows = $result->fetchall(PDO::FETCH_ASSOC);
-        echo "<h3>" . 'There are total of ' . count($rows) . ' records in Database' . "</h3>";
+                    <p><span class='error'>* required fields</span></p>
+                    <p><label for='event_title'>Event Name:- </label><input type='text' name='event_title' id='event_title' value=''><span class='error'>*</span></p>
+                    <p><label for='event_phone'>Phone:- </label><input type='text' name='event_phone' id='event_phone' value=''><span class='error'>*</span></p>
+                    <p><label for='event_date'>Date(dd/mm/yyyy):- </label><input type='text' name='event_date' id='event_date' value=''><span class='error'>*</span></p>
+                    <p><label for='event_shortBio'>Short Bio:- </label><textarea type='text' name='event_shortBio' id='event_shortBio'></textarea></p>
+                    <p><label for='event_longBio'>Long Bio:- </label><textarea type='text' name='event_longBio' id='event_longBio'></textarea></p>
+                    <p><label for='file'>Upload Icon/Logo:</label><input type='file' name='iconfile' id='iconfile' value=''><span class='error'>*</span></p>
+                    <p><label for='file'>Upload Image:</label><input type='file' name='imagefile' id='imagefile' value=''><span class='error'>*</span></p>
 
-        $editTally = 0;
-        foreach ($dbh->query($sql) as $row){
-            ++$editTally;
+                    <input type='submit' name='submit' id='submit' value='Add Event'>
 
-            //single echo added for all html code
-            echo "
+                    <div id='box' title='Things to note'>
+                        <label><p>Note:</p> </label>
+                        <p> - Individual upload for icon or photo not allowed. When adding record, both photo and icon must be uploaded on same time. </p>
+                    </div>
+
+                </fieldset>
+            </form>
+        </div>
+    ";
+
+    echo "
+        <div class='col w-2col m-2col'>
+            <h1>Current Event data/Information:</h1>
+    ";
+
+    $sql = "SELECT * FROM Events";
+    $result = $dbh->query($sql);
+    $rows = $result->fetchall(PDO::FETCH_ASSOC);
+
+    echo "
+            <h2>" . 'There are total of ' . count($rows) . ' records in Database' . "</h2>
+    ";
+
+    $editTally = 0;
+    foreach ($dbh->query($sql) as $row){
+        ++$editTally;
+
+        //single echo added for all html code
+        echo "
             <form id='eventRecord' name='eventRecord$editTally' method='post' enctype='multipart/form-data' action='";echo"$urlVar";echo"event_databaseProcess.php'>
                 <h4><label>Record ID: $row[event_id]</label></h4>
                 <input type='hidden' name='event_id' value='$row[event_id]' >
@@ -60,10 +67,14 @@ echo"
                     <img src='$row[event_promoPic]' width='100px'>
                 </p>
             </form>
-            ";
+        
+        ";
 
-        }
-        // closing database connection here
-        $dbh = null;
-        ?>
-</div>
+    }
+    echo"
+        </div>
+    ";
+    // closing database connection here
+    $dbh = null;
+?>
+
