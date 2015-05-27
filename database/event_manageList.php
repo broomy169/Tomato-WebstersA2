4116f58dca5cb76d0cecb4bd6f458f198f0c7a95
broomy169 <graeme.broomhall@my.jcu.edu.au>
2015-05-27 18:59:37 +1000

    take out 2 columns for events displaying

--- database/event_manageList.php
+++ database/event_manageList.php
@@ -49,7 +49,7 @@ if (!isset($_SESSION['user_email'])){
 
 	  //single echo added for all html code
 	  echo "
-	  <div class='col w-2col m-2col'>    
+	  <div>    
 		  <h1>Current Event Data/Information:</h1>
 		  <form id='eventRecord' name='eventRecord$editTally' method='post' enctype='multipart/form-data' action='database/event_databaseProcess.php'>
 			  <fieldset>
