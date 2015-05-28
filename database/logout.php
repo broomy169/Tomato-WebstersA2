cdb3ee7684695ae5cf43018833aaa93f7544b548
Parminder Singh <preetzdomain@gmail.com>
2015-05-28 19:29:37 +1000

    links fixed

--- database/logout.php
+++ database/logout.php
@@ -9,12 +9,11 @@ include($urlVar . "database_connect.php");
 //code to check and redirecting back to same page
 $url = $_GET["url"];
 
-//$url = $_SESSION["url"];
+$url = $_SESSION["url"];
 
 //setting up header link if using local machine server
-$check = "/Tomato-Webstersa2/";
-if ($url == $check."bands_addPage.php" || $url == $check."events_addPage.php" || $url == $check."message_addPage.php"
-            || $url == $check."users_addPage.php"){
+$check = "/Tomato-WebstersA2/";
+if ($url == $check."bands_addPage.php?pageName=editBands" || $url == $check."genre_addPage.php?pageName=editGenre" || $url == $check."events_addPage.php?pageName=editEvents" || $url == $check."message_addPage.php?pageName=editMessages" || $url == $check."users_addPage.php?pageName=editUsers"){
     $url = $check."index.php";
 }
 
@@ -23,6 +22,8 @@ if ($url == "/~tcmc03/bands_addPage.php?pageName=editBands" || $url == "/~tcmc03
     "/~tcmc03/events_addPage.php?pageName=editEvents" || $url == "/~tcmc03/message_addPage.php?pageName=editMessages" || $url == "/~tcmc03/users_addPage.php?pageName=editUsers"){
     $url = "/~tcmc03/index.php";
 }
+
+echo $url;
 
 // could use session variable for logout message but wanted to try that way as well :)
 header("Location: $url?status=loggedOut");
